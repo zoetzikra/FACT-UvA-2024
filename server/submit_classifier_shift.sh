afacf0d11bf1bfd6ea30a5c1dc11d45eba29da02
Jonas Schäfer <31080656+SchaefJo@users.noreply.github.com>
2024-02-04 00:03:40 +0100

    Add script to run classifier and shifted data

---
+++ server/submit_classifier_shift.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+classifiers=("logreg") #"rf" "SVC" "MLP" "adaboost"
+datasets=("3var-causal") # "3var-noncausal"
+confidences=("0.75" "0.95")
+
+for classifier in "${classifiers[@]}"
+do
+    for dataset in "${datasets[@]}"
+    do
+    	for confidence in "${confidences[@]}"
+    	do
+        	sbatch experiment.job ${dataset} 1000 100 ${confidence} 300 ../debugging/ 3 --NGEN 300 --POP_SIZE 150 --n_digits 1 --nr_refits 5 --predict_individualized True --model_type ${classifier} --robustness
+	done
+    done
+done
+
